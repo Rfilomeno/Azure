266aeba963f86df3135f98abb5579887396c3cd4
Rodrigo <rm.filomeno@gmail.com>
2017-06-07 12:31:24 -0300

    comentarios

--- Tp3-Azure.Service/GerenciamentoDeProdutoService.cs
+++ Tp3-Azure.Service/GerenciamentoDeProdutoService.cs
@@ -22,19 +22,19 @@ namespace Tp3_Azure.Service
         public void Create(Produto p)
         {
             fila.AdicionarProduto(p);
-            
+            //dao.Add(p); //é Realizado ao executar a fila
         }
 
         public void Delete(Guid produtoId)
         {
             fila.DeletarProduto(produtoId);
-            
+            // dao.Delete(produtoId); //é Realizado ao executar a fila
         }
 
         public void Edit(Guid produtoId, Produto p)
         {
             fila.EditarProduto(p);
-           
+            //dao.Edit(produtoId, p); //é Realizado ao executar a fila
         }
 
         public Produto Get(string nome)
